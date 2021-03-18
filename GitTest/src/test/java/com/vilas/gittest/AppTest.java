d53aa08b23ce04d459e5c7e2436d1cc691828052
Admin <Admin@192.168.0.5>
2021-03-18 12:47:03 +0530

    second commit

--- GitTest/src/test/java/com/vilas/gittest/AppTest.java
+++ GitTest/src/test/java/com/vilas/gittest/AppTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+//To Test the github configuration
 public class AppTest {
 	public WebDriver driver;
 	
